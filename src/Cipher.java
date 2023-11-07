9307eb0f05d095e983e5ff2735f625e2bf78919c
Beau Fontenot <104475813+bfontenot23@users.noreply.github.com>
2023-11-06 18:27:50 -0600

    Finished Project

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,2 +1,128 @@
-package PACKAGE_NAME;public class Cipher {
+public class Cipher {
+
+
+    /*
+    ASCII ranges:
+        Capital Letters: 65 - 90
+        Lowercase Letters: 97 - 122
+        Numbers: 48 - 57
+     */
+    private String message;
+    private int key;
+    private boolean encrypted;
+
+    /*
+     * Cipher - constructor for Cipher class
+     * @param message - string for the message of the cipher
+     * @param key - integer for the key of the cipher
+     */
+    public Cipher(String message, int key)
+    {
+        this.message = message;
+        this.key = key;
+        this.encrypted = false;
+    }
+
+
+    /*
+     * getMessage - gets the value of the message member
+     * @return - string of message member
+     */
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+    /*
+     * isEncrypted - gets the value of the encrypted member
+     * @return - boolean of encrypted member
+     */
+    public boolean isEncrypted()
+    {
+        return encrypted;
+    }
+
+    /*
+     * encrypt - encrypts the message member using the key
+     */
+    public void encrypt()
+    {
+        if(!encrypted)
+        {
+            char[] message = this.message.toCharArray();
+            for(int i=0; i<message.length;i++)
+            {
+                if(message[i]>=48&&message[i]<=57)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]++;
+                        if(message[i]>57) message[i] = 48;
+                    }
+                }
+                if(message[i]>=65&&message[i]<=90)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]++;
+                        if(message[i]>90) message[i] = 65;
+                    }
+                }
+                if(message[i]>=97&&message[i]<=122)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]++;
+                        if(message[i]>122) message[i] = 97;
+                    }
+                }
+            }
+            this.message = String.valueOf(message);
+            encrypted = true;
+        }
+        else System.out.print("This message is already encrypted.");
+    }
+
+    /*
+     * decrypt - decrypts the message member using the key
+     */
+    public void decrypt()
+    {
+        if(encrypted)
+        {
+            char[] message = this.message.toCharArray();
+            for(int i=0; i<message.length;i++)
+            {
+                if(message[i]>=48&&message[i]<=57)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]--;
+                        if(message[i]<48) message[i] = 57;
+                    }
+                }
+                if(message[i]>=65&&message[i]<=90)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]--;
+                        if(message[i]<65) message[i] = 90;
+                    }
+                }
+                if(message[i]>=97&&message[i]<=122)
+                {
+                    for(int j = 0; j<key; j++)
+                    {
+                        message[i]--;
+                        if(message[i]<97) message[i] = 122;
+                    }
+                }
+            }
+            this.message = String.valueOf(message);
+            encrypted = false;
+        }
+        else System.out.print("This message is already decrypted.");
+    }
+
+
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Driver {
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        String s1 = "Hello, World!";
+        String s2 = "Try this on for size!";
+        String s3 = "THIS IS SUPER SECRET";
+
+        Cipher c1 = new Cipher(s1, (char) 13);
+        Cipher c2 = new Cipher(s2,(char) 6);
+        Cipher c3 = new Cipher(s3,(char) 2);
+
+        c1.encrypt();
+        System.out.printf("[*]C1: %s\tC2: %s\t\tC3: %s\n",(c1.isEncrypted()) ? "Encrypted" : "Unencrypted",(c2.isEncrypted()) ? "Encrypted" : "Unencrypted",(c3.isEncrypted()) ? "Encrypted" : "Unencrypted");
+        c2.encrypt();
+        c3.encrypt();
+        System.out.printf("[*]Encrypting %s with a key of %d gives us %s\n", s1,13,c1.getMessage());
+        System.out.printf("[*]Encrypting %s with a key of %d gives us %s\n", s2,6,c2.getMessage());
+        System.out.printf("[*]Encrypting %s with a key of %d gives us %s\n", s3,2,c3.getMessage());
+
+        c1.decrypt();
+        c2.decrypt();
+        System.out.printf("[*]C1: %s\tC2: %s\t\tC3: %s\n",(c1.isEncrypted()) ? "Encrypted" : "Unencrypted",(c2.isEncrypted()) ? "Encrypted" : "Unencrypted",(c3.isEncrypted()) ? "Encrypted" : "Unencrypted");
+        c3.decrypt();
+        System.out.printf("[*]Now decrypted:\n");
+        System.out.printf("[*]C1 -> %s\n",c1.getMessage());
+        System.out.printf("[*]C2 -> %s\n",c2.getMessage());
+        System.out.printf("[*]C3 -> %s\n",c3.getMessage());
+
+    }
+
 }
